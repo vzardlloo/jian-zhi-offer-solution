9af5cb0e4746b5743f935731d7ccf34064f020ee
vzardlloo <vzardlloo@gmail.com>
2017-08-19 20:52:02 +0800

    jian-zhi-offer.iml

--- dep.sh
+++ dep.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+echo "                           "
+echo "                           "
 echo "***************************"
 echo "*we will do a deploy soon!*"
 echo "***************************"
 echo "                           "
 echo "                           "
+echo S@
 
 git add $1
 git commit -m "$2"
