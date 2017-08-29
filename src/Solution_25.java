120400bfb32082502945f42f7b5905c454da4c94
vzardlloo <vzardlloo@gmail.com>
2017-08-29 21:44:45 +0800

    Add solution

--- src/Solution_25.java
+++ src/Solution_25.java
@@ -11,8 +11,62 @@
  * Created by V-zar on 2017/8/23.
  */
 public class Solution_25 {
+    public RandomListNode Clone(RandomListNode pHead){
+
+        if (pHead == null){
+            return null;
+        }
+
+        //复制链表元素
+        RandomListNode pCur = pHead;
+        while (pCur != null){
+            RandomListNode node = new RandomListNode(pCur.label);
+            node.next = pCur.next;
+            pCur.next = node;
+
+            pCur = node.next;
+        }
+        //复制random指向
+        pCur = pHead;
+        while (pCur != null){
+            if (pCur.random != null){
+
+                pCur.next.random = pCur.random.next;
+                pCur = pCur.next.next;
+
+            }
+        }
+
+        RandomListNode head = pHead.next;
+        RandomListNode cur = head;
+        pCur = pHead;
+
+        //拆分链表
+        while (pCur != null){
+            pCur.next = pCur.next.next;
+            if (cur.next != null)  cur.next = cur.next.next;
+
+            cur = cur.next;
+            pCur = pCur.next;
+
+        }
 
 
 
+        return head;
+    }
 
+
+
+}
+
+
+class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
 }
